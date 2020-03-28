a5f8886a4ee5fd1f2ffecbec2cda54af2a6a2513
EnjoyDark <1220829207@qq.com>
2020-03-28 09:46:07 +0800

    fabric

--- blogproject/settings/local.py
+++ blogproject/settings/local.py
@@ -1,4 +1,4 @@
-from .commom import *
+from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'development-secret-key'  # 随便修改个名称
--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -1,4 +1,4 @@
-from .commom import *
+from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
