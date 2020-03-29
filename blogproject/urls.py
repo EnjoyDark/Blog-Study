ae91d26863e07f756c37091073134531e9761ffe
EnjoyDark <1220829207@qq.com>
2020-03-29 21:02:07 +0800

    small change

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from blog.feeds import AllPostsRssFeed
 from blog import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
